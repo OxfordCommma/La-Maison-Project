ee46b32c3d50e96bf49cf9f3e054fb0beb0f63ba
RamiroElianfa <ramirofarias4@gmail.com>
2024-06-01 01:58:06 -0300

    Se creó el archivo js que tendrá los productos

---
+++ js/arrayProductos.js
@@ -0,0 +1,20 @@
+const arrayProductos = [
+    // <----- Guitarras Eléctricas ------>
+    { id: 1, nombre: "Fender Stratocaster", imagen: "images/stratocaster.jpg", tipo: "Guitarra Eléctrica", precio: 150000 },
+    { id: 2, nombre: "Gibson Les Paul", imagen: "images/les_paul.jpg", tipo: "Guitarra Eléctrica", precio: 200000 },
+    { id: 3, nombre: "Ibanez RG", imagen: "images/ibanez_rg.jpg", tipo: "Guitarra Eléctrica", precio: 120000 },
+    { id: 4, nombre: "PRS Custom 24", imagen: "images/prs_custom_24.jpg", tipo: "Guitarra Eléctrica", precio: 250000 },
+    { id: 5, nombre: "Yamaha Pacifica", imagen: "images/pacifica.jpg", tipo: "Guitarra Eléctrica", precio: 90000 },
+    { id: 6, nombre: "Jackson Dinky", imagen: "images/jackson_dinky.jpg", tipo: "Guitarra Eléctrica", precio: 130000 },
+    { id: 7, nombre: "ESP Eclipse", imagen: "images/esp_eclipse.jpg", tipo: "Guitarra Eléctrica", precio: 220000 },
+    { id: 8, nombre: "Schecter Hellraiser", imagen: "images/hellraiser.jpg", tipo: "Guitarra Eléctrica", precio: 180000 },
+    // <----- Álbumes Musicales ------>
+    { id: 9, nombre: "Abbey Road - The Beatles", imagen: "images/abbey_road.jpg", tipo: "Álbum Musical", precio: 5000 },
+    { id: 10, nombre: "Dark Side of the Moon - Pink Floyd", imagen: "images/dark_side.jpg", tipo: "Álbum Musical", precio: 6000 },
+    { id: 11, nombre: "Thriller - Michael Jackson", imagen: "images/thriller.jpg", tipo: "Álbum Musical", precio: 7000 },
+    { id: 12, nombre: "Back in Black - AC/DC", imagen: "images/back_in_black.jpg", tipo: "Álbum Musical", precio: 6500 },
+    { id: 13, nombre: "Rumours - Fleetwood Mac", imagen: "images/rumours.jpg", tipo: "Álbum Musical", precio: 5500 },
+    { id: 14, nombre: "The Wall - Pink Floyd", imagen: "images/the_wall.jpg", tipo: "Álbum Musical", precio: 6000 },
+    { id: 15, nombre: "Led Zeppelin IV - Led Zeppelin", imagen: "images/led_zeppelin_iv.jpg", tipo: "Álbum Musical", precio: 6500 },
+    { id: 16, nombre: "Nevermind - Nirvana", imagen: "images/nevermind.jpg", tipo: "Álbum Musical", precio: 6000 },
+];
